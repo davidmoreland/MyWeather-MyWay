e910882464ef21213f362a0d39c251409814ff3b
David Moreland <dcmoreland@hotmail.com>
2021-03-01 11:54:08 -0600

    Initial Commit

---
+++ Shared/My_Weather_My_WayApp.swift
@@ -0,0 +1,17 @@
+//
+//  My_Weather_My_WayApp.swift
+//  Shared
+//
+//  Created by David Moreland on 3/1/21.
+//
+
+import SwiftUI
+
+@main
+struct My_Weather_My_WayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
